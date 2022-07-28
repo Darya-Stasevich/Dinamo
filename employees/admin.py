af51e62d97b8378bdd273ec0219aa0103e480615
Darya Stasevich <stasevich.daria@gmail.com>
2022-07-28 23:38:07 +0300

    added models: Management, EmployeeArticle

--- employees/admin.py
+++ employees/admin.py
@@ -1,1 +1,27 @@
+from django.contrib import admin
 
+from employees.models import ArticleAdditionalImage, EmployeeArticle, Management
+
+
+class ArticleAdditionalImageInline(admin.TabularInline):
+    """Класс для добавления в админке поля с дополнительными изображениями к модели News"""
+    model = ArticleAdditionalImage
+
+
+class EmployeeArticleAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name_employee",)}
+    list_display = ('name_employee', 'title', 'description', 'published', 'created', 'updated')
+    list_editable = ('published',)
+    list_filter = ('published', 'created')
+    search_fields = ('title', 'name_employee')
+    inlines = (ArticleAdditionalImageInline,)
+
+
+class ManagementAdmin(admin.ModelAdmin):
+    list_display = ('name', 'position', 'phone_number', 'email')
+    list_editable = ('phone_number',)
+    search_fields = ('name', 'position')
+
+
+admin.site.register(EmployeeArticle, EmployeeArticleAdmin)
+admin.site.register(Management, ManagementAdmin)
--- employees/models.py
+++ employees/models.py
@@ -1,29 +1,61 @@
-# from django.db import models
-#
-#
-# class Management(models.Model):
-#     """Руководство организации"""
-#     position = models.CharField(max_length=200, verbose_name="Должность")
-#     name = models.CharField(max_length=200, verbose_name="ФИО")
-#     description = models.TextField(verbose_name="Биография")
-#     phone_number = models.CharField('Номер телефона', max_length=30, blank=True)
-#     email = models.EmailField(blank=True, verbose_name='Электронная почта')
-#     email_for_citizens = models.EmailField(blank=True, verbose_name='Электронная почта для обращений граждан')
-#     image = models.ImageField(upload_to='management/', verbose_name="Изображение",
-#                               blank=True)
-#     info_visits = models.CharField(max_length=200, verbose_name="Информация о времени приема граждан")
-#
-#     class Meta:
-#         verbose_name = 'Руководство'
-#         verbose_name_plural = 'Руководство'
-#
-#     def __str__(self):
-#         return self.name
-#
+from django.db import models
 
-# class Worker(models.Model):
-#     pass
-#
-#
+
+class Management(models.Model):
+    """Руководство организации"""
+    position = models.CharField(max_length=200, verbose_name="Должность")
+    name = models.CharField(max_length=200, verbose_name="ФИО")
+    description = models.TextField(verbose_name="Биография", blank=True, null=True)
+    phone_number = models.CharField('Номер телефона для связи', max_length=30, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True, verbose_name='Электронная почта сотрудника')
+    email_for_citizens = models.EmailField(blank=True, null=True,
+                                           verbose_name='Электронная почта для обращений граждан')
+    image = models.ImageField(upload_to='management/', verbose_name="Фотография",
+                              blank=True, null=True)
+    info_visits = models.CharField(max_length=200, verbose_name="Информация о времени приема граждан")
+
+    class Meta:
+        verbose_name = 'Руководство'
+        verbose_name_plural = 'Руководство'
+
+    def __str__(self):
+        return self.name
+
+
+class EmployeeArticle(models.Model):
+    """Статья о сотруднике организации"""
+    name_employee = models.CharField(max_length=200, verbose_name="ФИО")
+    slug = models.SlugField(unique=True)
+    title = models.CharField('Название статьи', max_length=150, blank=True, null=True)
+    description = models.TextField(verbose_name="Биография", blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания статьи")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Дата редактирования статьи")
+    published = models.BooleanField('Опубликовать на сайте', default=True)
+    cover_image = models.ImageField(upload_to='employees/', verbose_name="Главная картинка для статьи про сотрудника",
+                                    blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Статью о сотруднике'
+        verbose_name_plural = 'Статьи о сотрудниках'
+
+    def __str__(self):
+        return f'{self.name_employee}, {self.title}'
+
+
+class ArticleAdditionalImage(models.Model):
+    """Дополнительные изображения к статье о сотруднике"""
+    article = models.ForeignKey(
+        EmployeeArticle,
+        on_delete=models.CASCADE,
+        verbose_name='Статья о сотруднике'
+    )
+    image = models.ImageField(upload_to='employees/', blank=True, null=True,
+                              verbose_name='Картинка для статьи о сотруднике')
+
+    class Meta:
+        verbose_name = 'Дополнительное изображение'
+        verbose_name_plural = 'Дополнительные изображения'
+
+
 # class Vacancy(models.Model):
 #     pass
