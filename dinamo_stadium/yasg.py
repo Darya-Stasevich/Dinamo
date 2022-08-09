134fd52b881759b83f5e655d6bcb11cc68bc3a8e
hinch32749 <alex.g.blyad@mail.ru>
2022-08-09 21:23:30 +0300

    and another one

--- dinamo_stadium/yasg.py
+++ dinamo_stadium/yasg.py
@@ -12,7 +12,7 @@ schema_view = get_schema_view(
         linecache=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=(permissions.AllowAny, ),
+    permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
