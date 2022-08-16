44ff7df21b7808d1f5bf27391f21be25214a80f7
Darya Stasevich <stasevich.daria@gmail.com>
2022-08-16 15:54:27 +0300

    modified serializers, added new API for EmployeeArticle

--- api_dinamo/serializers.py
+++ api_dinamo/serializers.py
@@ -1,26 +1,37 @@
 from rest_framework import serializers
 
+from employees.models import EmployeeArticle
 from news.models import News
 from services.models import Service, CategoryService
 
 
 class CategoryServiceSerializer(serializers.ModelSerializer):
     """Сериализатор для категории услуг"""
+
     class Meta:
         model = CategoryService
         fields = ['id', 'slug', 'title', ]
 
 
 class ServiceSerializer(serializers.ModelSerializer):
-    """Сериализатор для отображения услуг на первой странице"""
+    """Сериализатор для отображения услуг на ГЛАВНОЙ странице"""
+
     class Meta:
         model = Service
-        fields = ['id', 'slug', 'title', 'image', 'is_primary_service' ]
+        fields = ['id', 'slug', 'title', 'image', 'is_primary_service']
 
 
 class NewsSerializer(serializers.ModelSerializer):
-    """Сериализатор для отображения новостей на первой странице"""
+    """Сериализатор для отображения новостей на ГЛАВНОЙ странице"""
+
     class Meta:
         model = News
         fields = ['id', 'slug', 'title', 'brief_description', 'cover_image', ]
 
+
+class EmployeeArticleSerializer(serializers.ModelSerializer):
+    """Сериализатор для отображения статей про сотрудников на странице НАШИ СОТРУДНИКИ"""
+
+    class Meta:
+        model = EmployeeArticle
+        fields = ['id', 'slug', 'name_employee', 'title', 'created', 'cover_image', ]
--- vacancies/models.py
+++ vacancies/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Vacancy(models.Model):
     title_of_vacancy = models.CharField(max_length=100, verbose_name='Название вакансии', unique=True)
-    description = models.TextField()
+    description = models.TextField(verbose_name='Описание вакансии')
 
     class Meta:
         verbose_name_plural = 'Вакансии'
@@ -14,11 +14,15 @@ class Vacancy(models.Model):
 
 
 class MainInformationOfVacancy(models.Model):
-    title = models.CharField(max_length=100, verbose_name='Информация о вакансии')
+    title = models.CharField(max_length=100, verbose_name='Информация о вакансии',
+                             help_text='Например, З/П до 1000 руб., Требуемый опыт работы, Полная занятость и т.п.')
     vacancy = models.ForeignKey('Vacancy', on_delete=models.CASCADE, verbose_name='Вакансия')
 
 
 class VacancyRequirements(models.Model):
-    title = models.CharField(max_length=100, verbose_name='Название требования')
-    list_of_requirements = models.TextField(help_text=('Скопируйте этот тег в Пункты требования: &lt;li&gt;ВВЕДИТЕ ВАШ ТЕКСТ В ЭТОМ ТЕГЕ&lt;/li&gt;'), verbose_name='Пункты требования')
+    title = models.CharField(max_length=100, verbose_name='Название требования',
+                             help_text='Например, ТРЕБОВАНИЯ, ОБЯЗАННОСТИ, ПРИВЕТСТВУЕТСЯ, ОПЫТ РАБОТЫ и т.д.')
+    list_of_requirements = models.TextField(
+        help_text=('Скопируйте этот тег в Пункты требования: &lt;li&gt;ВВЕДИТЕ ВАШ ТЕКСТ В ЭТОМ ТЕГЕ&lt;/li&gt;'),
+        verbose_name='Пункты требования')
     vacancy = models.ForeignKey('Vacancy', on_delete=models.CASCADE, verbose_name='Вакансия')
